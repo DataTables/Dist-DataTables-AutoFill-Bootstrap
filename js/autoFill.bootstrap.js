cc988e3c6f53242d23b51a2449e6cef6154ac92f
dtbuild <dtbuild@rigil>
2026-07-22 12:45:30 +0000

    a8c27eb2c9ad9bdff5ab6b633234a23be141f865 Release - 3.0.0
    Sync to source repo @a8c27eb2c9ad9bdff5ab6b633234a23be141f865

--- js/autoFill.bootstrap.js
+++ js/autoFill.bootstrap.js
@@ -1,4 +1,4 @@
-/*! AutoFill Bootstrap 3 styling 3.0.0-beta.1 for DataTables
+/*! AutoFill Bootstrap 3 styling 3.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
--- js/autoFill.bootstrap.min.mjs
+++ js/autoFill.bootstrap.min.mjs
@@ -1,4 +1,4 @@
-/*! AutoFill Bootstrap 3 styling 3.0.0-beta.1 for DataTables
+/*! AutoFill Bootstrap 3 styling 3.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-bs";import AutoFill from"datatables.net-autofill";DataTable.AutoFill.classes.btn="btn btn-primary";export default DataTable;
